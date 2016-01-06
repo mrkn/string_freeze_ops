41afc0fb35117630158ff7c8d7b676c2fd0fcf51
Kenta Murata <mrkn@mrkn.jp>
2016-01-06 18:59:33 +0900

    Write spec file

--- spec/string_freeze_ops_spec.rb
+++ spec/string_freeze_ops_spec.rb
@@ -4,8 +4,58 @@ describe StringFreezeOps do
   it 'has a version number' do
     expect(StringFreezeOps::VERSION).not_to be nil
   end
+end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+describe String do
+  describe '#+@' do
+    context 'The receiver is frozen' do
+      let(:original) { 'string'.freeze }
+
+      specify do
+        expect(+original).not_to be_frozen
+      end
+
+      specify do
+        expect(+original).not_to equal(original)
+      end
+    end
+
+    context 'The receiver is not frozen' do
+      let(:original) { 'string' }
+
+      specify do
+        expect(+original).not_to be_frozen
+      end
+
+      specify do
+        expect(+original).to equal(original)
+      end
+    end
+  end
+
+  describe '#-@' do
+    context 'The receiver is frozen' do
+      let(:original) { 'string'.freeze }
+
+      specify do
+        expect(-original).to be_frozen
+      end
+
+      specify do
+        expect(-original).to equal(original)
+      end
+    end
+
+    context 'The receiver is not frozen' do
+      let(:original) { 'string' }
+
+      specify do
+        expect(-original).to be_frozen
+      end
+
+      specify do
+        expect(-original).not_to equal(original)
+      end
+    end
   end
 end
